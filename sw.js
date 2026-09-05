d54918a10fdf3774608223b9f9755073f5e64fea
infokusima <info.kusima@gmail.com>
2026-09-05 12:54:29 +0200

    Bump DJ Choice web cache to V2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="djchoice-web-v1-shell";
+const CACHE="djchoice-web-v2-shell";
 const SHELL=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)));
@@ -11,5 +11,9 @@ self.addEventListener("activate",e=>{
 self.addEventListener("fetch",e=>{
   const u=new URL(e.request.url);
   if(u.origin!==location.origin) return;
-  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request)));
+  e.respondWith(fetch(e.request).then(r=>{
+    const copy=r.clone();
+    caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
+    return r;
+  }).catch(()=>caches.match(e.request)));
 });
